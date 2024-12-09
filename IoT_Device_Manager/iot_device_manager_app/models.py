eadb8af6b691d837229f9c81a0a39c3d69c5aa84
GameKnightt <alexy.l@sfr.fr>
2024-12-09 12:01:22 +0100

    add forms

--- IoT_Device_Manager/iot_device_manager_app/models.py
+++ IoT_Device_Manager/iot_device_manager_app/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Sensor(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
-    longi = models.IntegerField()  # Longitude
-    lati = models.IntegerField()  # Latitude
+    longi = models.FloatField()  # Longitude
+    lati = models.FloatField()  # Latitude
     period = models.IntegerField()
 
 class Measure(models.Model):
--- IoT_Device_Manager/iot_device_manager_app/views.py
+++ IoT_Device_Manager/iot_device_manager_app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Sensor, Unit
+from .forms import SensorForm, UnitForm
 
-# Create your views here.
 def sensor_list(request):
     sensors = Sensor.objects.all()
     return render(request, 'sensor_list.html', {'sensors': sensors})
@@ -11,9 +11,65 @@ def unit_list(request):
     return render(request, 'unit_list.html', {'units': units})
 
 def sensor_detail(request, pk):
-    sensor = Sensor.objects.get(pk=pk)
+    sensor = get_object_or_404(Sensor, pk=pk)
     return render(request, 'sensor_detail.html', {'sensor': sensor})
 
 def unit_detail(request, pk):
-    unit = Unit.objects.get(pk=pk)
+    unit = get_object_or_404(Unit, pk=pk)
     return render(request, 'unit_detail.html', {'unit': unit})
+
+def sensor_create(request):
+    if request.method == 'POST':
+        form = SensorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sensor_list')
+    else:
+        form = SensorForm()
+    return render(request, 'sensor_form.html', {'form': form})
+
+def unit_create(request):
+    if request.method == 'POST':
+        form = UnitForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('unit_list')
+    else:
+        form = UnitForm()
+    return render(request, 'unit_form.html', {'form': form})
+
+def sensor_update(request, pk):
+    sensor = get_object_or_404(Sensor, pk=pk)
+    if request.method == 'POST':
+        form = SensorForm(request.POST, instance=sensor)
+        if form.is_valid():
+            form.save()
+            return redirect('sensor_detail', pk=sensor.pk)
+    else:
+        form = SensorForm(instance=sensor)
+    return render(request, 'sensor_form.html', {'form': form})
+
+def unit_update(request, pk):
+    unit = get_object_or_404(Unit, pk=pk)
+    if request.method == 'POST':
+        form = UnitForm(request.POST, instance=unit)
+        if form.is_valid():
+            form.save()
+            return redirect('unit_detail', pk=unit.pk)
+    else:
+        form = UnitForm(instance=unit)
+    return render(request, 'unit_form.html', {'form': form})
+
+def sensor_delete(request, pk):
+    sensor = get_object_or_404(Sensor, pk=pk)
+    if request.method == 'POST':
+        sensor.delete()
+        return redirect('sensor_list')
+    return render(request, 'sensor_confirm_delete.html', {'sensor': sensor})
+
+def unit_delete(request, pk):
+    unit = get_object_or_404(Unit, pk=pk)
+    if request.method == 'POST':
+        unit.delete()
+        return redirect('unit_list')
+    return render(request, 'unit_confirm_delete.html', {'unit': unit})
